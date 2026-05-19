750a80ac5e440bf63b47fccf95c1a949861a52a4
PiyushGrubpac <jha.piyush@grubpac.com>
2026-05-19 12:31:49 +0530

    Add comprehensive seed data with standard roles and fix missing permission topics

--- src/modules/common/handlers/permissions/get-permissions.handler.ts
+++ src/modules/common/handlers/permissions/get-permissions.handler.ts
@@ -1,11 +1,15 @@
 import { createHandlers } from "@/utils/hono-factory.ts";
 import {
+	BOX_VERTICALS,
 	CLIENTS_PERMISSIONS,
 	DASHBOARD_PERMISSIONS,
 	EMPLOYEES_PERMISSIONS,
+	GRUBPACS_PERMISSIONS,
 	PERMISSION_TOPICS,
 	ROLES_PERMISSIONS,
 	SUPPORT_PERMISSIONS,
+	SYSTEM_SETTINGS_PERMISSIONS,
+	VERTICALS_PERMISSIONS,
 } from "@/configs/constants.ts";
 import type { APIResponse } from "@/types/api";
 
@@ -20,6 +24,9 @@ export const getPermissionsHandler = createHandlers(async (context) => {
 		[PERMISSION_TOPICS.ROLES]: Object.values(ROLES_PERMISSIONS),
 		[PERMISSION_TOPICS.CLIENTS]: Object.values(CLIENTS_PERMISSIONS),
 		[PERMISSION_TOPICS.SUPPORT]: Object.values(SUPPORT_PERMISSIONS),
+		[PERMISSION_TOPICS.GRUBPACS]: Object.values(GRUBPACS_PERMISSIONS),
+		[PERMISSION_TOPICS.SYSTEM_SETTINGS]: Object.values(SYSTEM_SETTINGS_PERMISSIONS),
+		verticals: [...BOX_VERTICALS, ...Object.values(VERTICALS_PERMISSIONS)],
 	};
 
 	return context.json<APIResponse<ResponseData>>({
